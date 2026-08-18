630ebd151a41e5a207f481944f45697738cc56cf
Ankit Jain <ankit@aviator.co>
2026-08-18 03:44:32 +0000

    shared: introduce Config type (#8)

--- packages/shared/src/lib/shared.ts
+++ packages/shared/src/lib/shared.ts
@@ -1,3 +1,12 @@
 export function shared(): string {
   return 'shared';
 }
+
+export interface Config {
+  env: 'dev' | 'prod';
+  region: string;
+}
+
+export function defaultConfig(): Config {
+  return { env: 'dev', region: 'us-west-2' };
+}
